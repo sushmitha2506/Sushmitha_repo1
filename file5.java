ff5586e4a29458d3a2f98fdef4253f3236c13150
sushmitha2506 <101458391+sushmitha2506@users.noreply.github.com>
2022-07-28 12:18:26 +0530

    file5 commit: to check fetch command

---
+++ file5.java
@@ -0,0 +1,13 @@
+Git Merge
+=============
+Git merge is used to merge files of two different branches.
+
+git merge <branchName>
+
+
+Git  rebase
+==============
+rebase is nothing but a merge, but commit history will gets added on top of the branch.
+Rebase is also called as fastforward merge.
+The commits from the child branch are added to the top of the master branch.
+with rebase we can modify the commit history
